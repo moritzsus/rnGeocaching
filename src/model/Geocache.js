83939126015ea597778928398f68f2ae6f2a6e5c
Moritz Süssel <suessel.moritz@web.de>
2024-01-16 14:09:00 +0100

    added geocache database and added 10 geocaches
    SQLiteService.js in model: Handles database creation and has functions for adding and getting geocaches; Geocache.js in model: structure of a geocache; GeocacheViewModel in viewmodel: initializes databse with 10 geocaches; at the moment the database infos are fetched when navigating to InfoScreen and the names of the caches are being displayed

---
+++ src/model/Geocache.js
@@ -0,0 +1,13 @@
+class Geocache {
+    constructor(name, latitude, longitude, elevation, isFound, foundTime) {
+      this.name = name;
+      this.latitude = latitude;
+      this.longitude = longitude;
+      this.elevation = elevation;
+      this.isFound = isFound;
+      this.foundTime = foundTime;
+    }
+  }
+  
+  export default Geocache;
+  
--- src/view/screens/InfoScreen.js
+++ src/view/screens/InfoScreen.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import GeocacheViewModel from "../../viewmodel/GeocacheViewModel";
 
 const InfoScreen = () => {
+  const [geocaches, setGeocaches] = useState([]);
+
+  useEffect(() => {
+    fetchGeocaches();
+  }, []);
+
+  const fetchGeocaches = async () => {
+    try {
+      await GeocacheViewModel.initializeDatabase();
+      const geocachesData = await GeocacheViewModel.getGeocaches();
+      setGeocaches(geocachesData);
+    } catch (error) {
+      console.error("Fehler beim Abrufen der Geocaches", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>InfoScreen</Text>
+      <Text style={styles.header}>InfoScreen</Text>
+      <FlatList
+        data={geocaches}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.geocacheContainer}>
+            <Text style={styles.geocacheName}>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -12,12 +38,21 @@ const InfoScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  text: {
+  header: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  geocacheContainer: {
+    marginBottom: 10,
+  },
+  geocacheName: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
 
